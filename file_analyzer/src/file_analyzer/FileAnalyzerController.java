31c57148e9f9b6f4770d31d06688a72cf0a1653d
Trinh Nguyen <trinh.d.nguyen@sjsu.edu>
2016-10-06 18:34:37 -0700

    Read in binary feature
    Converts binary file to show ascii characters

--- file_analyzer/src/file_analyzer/FileAnalyzerController.java
+++ file_analyzer/src/file_analyzer/FileAnalyzerController.java
@@ -8,10 +8,9 @@ import java.io.FileNotFoundException;
 import java.io.FileOutputStream;
 import java.io.IOException;
 import java.io.PrintStream;
-import java.nio.file.FileSystems;
+import java.nio.charset.Charset;
 import java.nio.file.Files;
-import java.nio.file.Path;
-import java.nio.file.attribute.BasicFileAttributes;
+import java.nio.file.Paths;
 
 import javax.swing.JFileChooser;
 
@@ -19,6 +18,7 @@ public class FileAnalyzerController {
 
 	private File _file;
 	private FileAnalyzerView _view;
+	private String[] _asciiArray;
 	private String _outputString = "";
 
 	public FileAnalyzerController(FileAnalyzerView view) {
@@ -30,7 +30,7 @@ public class FileAnalyzerController {
 		return _file;
 	}
 
-	public void openFile(JFileChooser fileChooser) {
+	public void openFile(JFileChooser fileChooser) throws IOException {
 		int returnVal = fileChooser.showOpenDialog(_view);
 
 		if (returnVal == JFileChooser.APPROVE_OPTION) {
@@ -38,17 +38,66 @@ public class FileAnalyzerController {
 
 			_view.getFileLabel().setText(_file.getName());
 
-			Path path = FileSystems.getDefault().getPath(_file.getAbsolutePath());
-			try {
-				BasicFileAttributes attr = Files.readAttributes(path, BasicFileAttributes.class);
-				_outputString = "Name: " + _file.getName() + "\n"
-						+ "CreationTime: " + attr.creationTime() + "\n"
-						+ "... more file metadata here";
-				_view.getTextArea().setText(_outputString);
-			} catch(IOException e) {
-				System.out.println(e);
+			_outputString = convertToBinary();
+			_asciiArray = binaryToAscii(_outputString);
+			_outputString = asciiArrayToString(_asciiArray);
+			_view.getTextArea().setText(_outputString);
+		}
+
+	}
+
+	private String convertToBinary() throws IOException {
+		byte[] encoded = Files.readAllBytes(Paths.get(_file.getAbsolutePath()));
+		String tempString = new String(encoded, Charset.defaultCharset());
+
+		byte[] bytes = tempString.getBytes();
+		StringBuilder binary = new StringBuilder();
+		for (byte b : bytes) {
+			int val = b;
+			for (int i = 0; i < 8; i++) {
+				binary.append((val & 128) == 0 ? 0 : 1);
+				val <<= 1;
 			}
 		}
+		return binary.toString();
+	}
+
+	private String[] binaryToAscii(String s) {
+		int arrayLength = (int) Math.ceil(((s.length() / (double)8)));
+		String[] result = new String[arrayLength];
+
+		int j = 0;
+		int lastIndex = result.length - 1;
+		for (int i = 0; i < lastIndex; i++) {
+			result[i] = s.substring(j, j + 8);
+			j += 8;
+		}
+		String lastString = s.substring(j);
+		StringBuilder sb = new StringBuilder();
+		if (lastString.length() < 9) {
+			int rest = 9 - lastString.length();
+			for(int i = 1; i < rest; i++) {
+				sb.append("0");
+			}
+			sb.append(lastString);
+		}
+		result[lastIndex] = sb.toString();
+		StringBuilder sb2 = new StringBuilder();
+		for (int i = 0; i < result.length; i++) {
+			int charCode = Integer.parseInt(result[i], 2);
+			sb2.append(new Character((char)charCode).toString());
+		}
+		String stringWithNonAscii = sb2.toString();
+		String stringWithOnlyAscii = stringWithNonAscii.replaceAll("\\P{InBasic_Latin}", "");
+		return stringWithOnlyAscii.split("(?!^)");
+	}
+	
+	private String asciiArrayToString(String[] asciiArray) {
+		StringBuilder sb = new StringBuilder();
+		for (String e : asciiArray) {
+		    sb.append(e);
+		}
+		return sb.toString();
 	}
 
 	public void saveFile(JFileChooser fileChooser) throws FileNotFoundException {
